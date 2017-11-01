2a20646d9e06d456fe7066695e6c98fa31834422
Akh <ai00038@outlook.com>
2017-11-01 18:32:29 +0000

    user object creation, user constructor not working

--- application/models/Student.php
+++ application/models/Student.php
@@ -1,9 +1,24 @@
 <?php
 
 class Student extends User {
-    //put your code here
+
+    private $supervisor_ID;
     
-        public function insertStudent($username, $pass1, $pass2, $userType) {
+    public function __construct($user_ID, $userTypeID, $firstName, $lastName, $email, $username, $supervisorID) {
+        parent::__construct($user_ID, $userTypeID, $firstName, $lastName, $email, $username);
+        $this->supervisor_ID = $supervisorID;
+    }
+    
+    function getSupervisor_ID() {
+        return $this->supervisor_ID;
+    }
+
+    function setSupervisor_ID($supervisor_ID) {
+        $this->supervisor_ID = $supervisor_ID;
+    }
+
+        
+    public function insertStudent($username, $pass1, $pass2, $userType) {
         print_r($_POST);
         echo"<br>";
         if (self::isUserUnique($username) == true && self::doesPasswordsMatch($pass1, $pass2) == true) {
@@ -11,24 +26,24 @@ class Student extends User {
             self::createAccount($username, $userType);
         }
     }
-    
-        public function createAccount($username, $userType){
-                $data1 = array(
-                'userType_ID' => $userType,
-                'firstName' => $this->input->post('stFirstName'),
-                'lastName' => $this->input->post('stLastName'),
-                'emailAddress' => $this->input->post('stEmail'),
-                'username' => $username,
-                'password' => self::encrypt("theSecretKeyInit", $this->input->post('stpwd1')),
-            );
-            $this->db->insert('fyp_User', $data1);
 
-            $query = $this->db->query("SELECT user_ID FROM fyp_User WHERE username='$username'");
-            $userID = $query->row();
-            $data2 = array(
-                'User_ID' => $userID->user_ID
-            );
-            $this->db->insert('fyp_Student', $data2);
+    public function createAccount($username, $userType) {
+        $data1 = array(
+            'userType_ID' => $userType,
+            'firstName' => $this->input->post('stFirstName'),
+            'lastName' => $this->input->post('stLastName'),
+            'emailAddress' => $this->input->post('stEmail'),
+            'username' => $username,
+            'password' => self::encrypt("theSecretKeyInit", $this->input->post('stpwd1')),
+        );
+        $this->db->insert('fyp_User', $data1);
+
+        $query = $this->db->query("SELECT user_ID FROM fyp_User WHERE username='$username'");
+        $userID = $query->row();
+        $data2 = array(
+            'User_ID' => $userID->user_ID
+        );
+        $this->db->insert('fyp_Student', $data2);
     }
-    
+
 }
--- application/models/User.php
+++ application/models/User.php
@@ -2,23 +2,69 @@
 
 class User extends CI_Model {
 
-    //put your code here, attributes
-//    public function __construct() {
-//        parent::__construct();
-//    }
+    private $user_ID;
+    private $userTypeID;
+    private $firstName;
+    private $lastName;
+    private $email;
+    private $username;
+    
+    function __construct() {
+        parent::__construct();
+    }
 
+    
+//    function __construct($user_ID, $userTypeID, $firstName, $lastName, $email, $username) {
+//        parent::__construct();
+//        $this->user_ID = $user_ID;
+//        $this->userTypeID = $userTypeID;
+//        $this->firstName = $firstName;
+//        $this->lastName = $lastName;
+//        $this->email = $email;
+//        $this->username = $username;
+//    }
+    
     public function authenticateUser($username, $password) {
         $encrPassword = self::encrypt("theSecretKeyInit", $password);
         $query = $this->db->query("SELECT * FROM fyp_User WHERE username='$username' AND password='$encrPassword'");
         if ($query->num_rows() === 1) {
+            $oneAccount = $query->row();
             $newQuery = $this->db->query("UPDATE fyp_User SET last_login = NOW() WHERE username='$username';");
-            if ($newQuery === true){
-            echo "Record updated, Successfully logged in";
+            if ($newQuery === true) {
+                echo "Record updated, Successfully logged in<br>";
+                $newUser = new User();
+                $newUser->setUser_ID($oneAccount->user_ID);
+                $newUser->setUserTypeID($oneAccount->userType_ID);
+                $newUser->setFirstName($oneAccount->firstName);
+                $newUser->setLastName($oneAccount->lastName);
+                $newUser->setEmail($oneAccount->emailAddress);
+                $newUser->setUsername($oneAccount->username);
+//                $newUser = new User(
+//                        $oneAccount->user_ID,
+//                        $oneAccount->userType_ID,
+//                        $oneAccount->firstName,
+//                        $oneAccount->lastName,
+//                        $oneAccount->emailAddress,
+//                        $oneAccount->username);
+                echo $newUser->getFirstName()." ".$newUser->getLastName()." ".$newUser->getUsername();
+                //self::sendToDashboard($oneAccount->userType_ID);
             } else {
                 echo "Error updating record";
             }
         } else {
             echo "Incorrect Username or Password, Try again";
+        }
+    }
+
+    public static function sendToDashboard($userType) {
+        if ($userType == 1) {
+            redirect("staff-home?type=ml");
+        } else if ($userType == 2) {
+            redirect("staff-home");
+        } else if ($userType == 3) {
+            redirect("student-home");
+        } else {
+            echo "userType is ".$userType.", which is unexpected";
         }
     }
 
@@ -47,5 +93,52 @@ class User extends CI_Model {
         $plain = base64_encode($plain);
         return substr($plain, 0, 44);
     }
+    
+    function getUser_ID() {
+        return $this->user_ID;
+    }
 
+    function getUserTypeID() {
+        return $this->userTypeID;
+    }
+
+    function getFirstName() {
+        return $this->firstName;
+    }
+
+    function getLastName() {
+        return $this->lastName;
+    }
+
+    function getEmail() {
+        return $this->email;
+    }
+
+    function getUsername() {
+        return $this->username;
+    }
+
+    function setUser_ID($user_ID) {
+        $this->user_ID = $user_ID;
+    }
+
+    function setUserTypeID($userTypeID) {
+        $this->userTypeID = $userTypeID;
+    }
+
+    function setFirstName($firstName) {
+        $this->firstName = $firstName;
+    }
+
+    function setLastName($lastName) {
+        $this->lastName = $lastName;
+    }
+
+    function setEmail($email) {
+        $this->email = $email;
+    }
+
+    function setUsername($username) {
+        $this->username = $username;
+    }
 }
