d878f215583e90c7c41295311eb95dd0bd028a64
Kashif <k.ismael@hotmail.co.uk>
2017-11-08 19:05:31 +0000

    Remember, SELECT queries return DB objects, not true or false.
    Don't try and do '===' on a select statement

--- application/models/User.php
+++ application/models/User.php
@@ -57,20 +57,12 @@ class User extends CI_Model {
     public function getSecondID($userID, $userType) {
         if ($userType == 1 || $userType == 2) {
             $firstquery = $this->db->query("SELECT * FROM fyp_Staff WHERE user_ID='$userID'");
-            if ($firstquery == true) {
                 $firstRow = $firstquery->row();
                 $this->session->set_userdata('secondaryID', $firstRow->staff_ID);
-            } else {
-                echo "query doesnt work";
-            }
         } else if ($userType == 3) {
             $firstquery = $this->db->query("SELECT * FROM fyp_Student WHERE user_ID='$userID'");
-            if ($firstquery == true) {
                 $firstRow = $firstquery->row();
                 $this->session->set_userdata('secondaryID', $firstRow->student_ID);
-            } else {
-                echo "query doesnt work";
-            }
         } else {
             echo "userType is " . $userType . ", which is unexpected";
         }
